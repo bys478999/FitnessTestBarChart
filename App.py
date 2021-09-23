d265c2f27a224322bec7e762e92cf07a842f56f8
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-09-23 09:02:18 +0800

    Add files via upload

---
+++ App.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from Multi_Page_App import MultiApp
+from APP import Home_Page, Second_Page
+
+app = MultiApp()
+
+st.markdown('Fitness Test Web Application')
+
+app.add_app("Bar Chart Maker", Home_Page.app)
+app.add_app("Bar Chart Maker (Data Comparison)", Second_Page.app)
+
+app.run()
+
