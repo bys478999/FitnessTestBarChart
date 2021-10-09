6601c74300cfbf7f86374f7b62d0d21f23eab48d
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-09 17:46:38 +0800

    Update App.py

--- App.py
+++ App.py
@@ -16,8 +16,8 @@ def main():
         password = st.sidebar.text_input("Password", type='password')
         if st.sidebar.button("Login"):
             if password == '1234' and username == 'abu':
-                st.success("Logged In as {}".format(username))
-                while app = MultiApp()
+                while st.success("Logged In as {}".format(username))
+                     app = MultiApp()
                     st.title('Fitness Test Web Application')
                     app.add_app("Bar Chart Maker", Home_Page.app)
                     app.add_app("Bar Chart Maker (Data Comparison)", Second_Page.app)
