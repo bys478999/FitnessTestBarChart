0b90440a3e9d458e70630ab5180f0181368c1df3
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-10 09:46:22 +0800

    Update App.py

--- App.py
+++ App.py
@@ -23,8 +23,10 @@ def main():
                  <p><h2 style = "color:black";>Aim</h2></p>
                  <p><h3 style = "color:orange";>Improve athlete's performance is our mission</h3></p>   
                  """,scrolling=False) 
-                
-                st.video("https://drive.google.com/file/d/1vZFUPynzEGHr_J6oqD_KUsbLBreoUfeB/view?usp=sharing")
+                video_file = open("https://drive.google.com/file/d/1vZFUPynzEGHr_J6oqD_KUsbLBreoUfeB/view?usp=sharing", 'rb')
+                video_bytes = video_file.read()
+                st.video(video_bytes)
+                st.video('https://drive.google.com/file/d/1vZFUPynzEGHr_J6oqD_KUsbLBreoUfeB/view?usp=sharing')
                 st.markdown('Our Aim:')
                 st.markdown("Improve athlete's performance is our mission")
                 image = Image.open('ussps.jpg')
