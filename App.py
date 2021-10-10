0598b63c02c2843fce36b18fbc70807cea0b1bc6
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-10 08:52:21 +0800

    Update App.py

--- App.py
+++ App.py
@@ -20,11 +20,11 @@ def main():
             if password == '123' and username == 'abu':
                 components.html(""" 
                  <h1 style = "color:yellow; background-color:blue" >Welcome to Sports Science & Sports Medicine</h1>
-                 <p><h2 style = "color:blue";>Aim</h2></p>
-                 <p><h3 style = "color:blue";>Improve athlete's performance is our mission</h3></p>  
+                 <p><h2 style = "color:black";>Aim</h2></p>
+                 <p><h3 style = "color:orange";>Improve athlete's performance is our mission</h3></p>  
                  """,scrolling=False)           
                 image = Image.open('ussps.jpg')
-                st.image(image, caption='Photo of intership students')
+                st.image(image, caption='Photo with intership students')
                
             else:
                 st.warning("Incorrect username/password")
