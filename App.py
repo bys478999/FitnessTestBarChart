ad622172e72acf679a2063c03fb7cc14ab3518a1
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-10 17:09:02 +0800

    Update App.py

--- App.py
+++ App.py
@@ -4,6 +4,8 @@ from APP import Home_Page, Second_Page
 import streamlit.components.v1 as components
 from PIL import Image
 
+st.write("DB username:", st.secrets["DB_USERNAME"])
+st.write("DB password:", st.secrets["DB_PASSWORD"])
 
 def main():
     st.title("""Sports Science & Sports Medicine Website""")
