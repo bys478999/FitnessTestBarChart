6fdd8a2e477db50ac0610f2e201f4cd07656d189
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-10 08:55:43 +0800

    Update App.py

--- App.py
+++ App.py
@@ -13,7 +13,6 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Home":
-        st.subheader("Home")
         username = st.sidebar.text_input("Username")
         password = st.sidebar.text_input("Password", type='password')
         if st.sidebar.checkbox("Login"): 
@@ -21,12 +20,8 @@ def main():
                 components.html(""" 
                  <h1 style = "color:yellow; background-color:blue" >Welcome to Sports Science & Sports Medicine</h1>
                  <p><h2 style = "color:black";>Aim</h2></p>
-                 <p><h3 style = "color:orange";>Improve athlete's performance is our mission</h3></p>  
-                 
-                 <h1 style = "color:yellow; background-color:blue" >Welcome to Sports Science & Sports Medicine</h1>
-                 <p><h2 style = "color:black";>Aim</h2></p>
-                 <p><h3 style = "color:orange";>Improve athlete's performance is our mission</h3></p>  
-                 """,scrolling=True)           
+                 <p><h3 style = "color:orange";>Improve athlete's performance is our mission</h3></p>   
+                 """,scrolling=False)           
                 image = Image.open('ussps.jpg')
                 st.image(image, caption='Photo with intership students')
                
