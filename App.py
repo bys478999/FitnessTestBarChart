807f3c389f97edcf2a95ef7f96b6aa06366491d6
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-16 21:42:29 +0800

    Update App.py

--- App.py
+++ App.py
@@ -52,10 +52,12 @@ def main():
                 st.warning("Incorrect username/password")
 
     elif choice == "Service":
+        sheet_id = '1NVPrXAes46nxhhevFzuvRkUCM9Y6JQNqUteR9KUyL-I'
         st.header("BodyMechanics Service")
-        df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/1NVPrXAes46nxhhevFzuvRkUCM9Y6JQNqUteR9KUyL-I/export?format=xlsx")
+        df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx")
                 st.subheader('Gym Workout Log')
                 st.dataframe(df)
+              
 
 if __name__ == '__main__':
     main()
