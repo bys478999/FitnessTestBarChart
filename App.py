8fa229c993a21397f774fc50a04316c67b92610b
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 11:01:52 +0800

    Update App.py

--- App.py
+++ App.py
@@ -70,7 +70,7 @@ def main():
                    selected_year = df.loc[df['Year'] == year]
                    b= selected_year.groupby(['Month'])['Name'].count()
                    x = selected_year['Month']
-                   st.write(b)
+                   st.write(x)
                    #fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))   
                    #y = b
                    #ax.plot(x,y)
