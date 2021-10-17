4cd2ee8ef46ae3c38b2ff882636103dc82006f39
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 14:22:38 +0800

    Update App.py

--- App.py
+++ App.py
@@ -108,7 +108,7 @@ def main():
                    #ax[0].legend(mylabels, loc='best')
                    #ax[1].legend(mylabels1, loc='best')
                    #ax[2].legend(mylabels2, loc='best')
-                   fig.legend(mylabels,mylabels1,mylabels2, loc='best')
+                   fig.legend([mylabels,mylabels1,mylabels2], loc='best')
                    fig.tight_layout()
                    st.pyplot(fig)
                    
