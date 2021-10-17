64feb6eaa627499fa0061daeffb612855c8fb84f
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 11:17:46 +0800

    Update App.py

--- App.py
+++ App.py
@@ -65,17 +65,17 @@ def main():
                    a= df.groupby(['Year'])['Month'].count()
                    st.write(a)
                                 
-                   year = st.selectbox('Chose The Year', df['Year'].drop_duplicates())
+                   year = st.selectbox('Chose The Year', df['Year'].drop_duplicates().astype(str))
                    col1, col2 = st.columns(2)
                    selected_year = df.loc[df['Year'] == year]
                    selected = selected_year.rename(columns={'Name': 'Case'})
                    b= selected.groupby(['Month'])['Case'].count()
                    x = selected['Month'].drop_duplicates()
                    col1.table(b)
-                   fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 4))   
+                   fig, ax = plt.subplots(nrows=1, ncols=1)   
                    y = b
                    ax.plot(x,y)
-                   ax.set_title("Cases in" + year)
+                   ax.set_title("Cases in " + year)
                    ax.set_xlabel('Month')
                    ax.set_ylabel('Case')
                    col2.pyplot(fig)
