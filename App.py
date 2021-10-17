d07ebba4a220a783e818b89b00c308a663c83781
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 13:19:15 +0800

    Update App.py

--- App.py
+++ App.py
@@ -99,9 +99,10 @@ def main():
                    y1 = selected_month.groupby(['Gender'])['Month'].count()
                    y2 = selected_month.groupby(['Status'])['Month'].count()
                    mylabels = selected_month['Sport'].drop_duplicates()
-                   ax[0].pie(y, labels=y)
+                   aa1 = ax[0].pie(y, labels=y)
                    ax[1].pie(y1,labels=y1)
                    ax[2].pie(y2,labels=y2)
+                   ax[0].legend([aa1],labels=[selected_month['Sport'].drop_duplicated()])
                 
                    st.pyplot(fig)
                     
