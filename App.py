c4da4f81d5462da215827dae3ca5bfad393dbc0e
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 20:12:32 +0800

    Update App.py

--- App.py
+++ App.py
@@ -111,7 +111,7 @@ def main():
                    fig.tight_layout()
                    st.pyplot(fig)
                    
-                   fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))    
+                   fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))    
                    ax.pie(y,labels=y)
                    ax.legend(mylabels, loc='best', bbox_to_anchor=(1.05, 1.0))
                    fig.tight_layout()
