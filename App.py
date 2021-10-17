cccaccab38171035c60dabedc575193cadaff7d2
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-17 14:16:47 +0800

    Update App.py

--- App.py
+++ App.py
@@ -105,7 +105,7 @@ def main():
                    ax[0].pie(y, labels=y)
                    ax[1].pie(y1,labels=y1)
                    ax[2].pie(y2,labels=y2)
-                   ax[0].legend(mylabels, loc='best')
+                   ax[0].legend(mylabels, loc='best',bbox_to_anchor=(0.5, -0.05)
                    ax[1].legend(mylabels1, loc='best')
                    ax[2].legend(mylabels2, loc='best')
                    fig.tight_layout()
