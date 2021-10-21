eddf8f55223405b054b52adac949097921b33262
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-10-21 12:17:56 +0800

    Update App.py

--- App.py
+++ App.py
@@ -138,7 +138,8 @@ def main():
                     sheet_id = st.secrets[username]
                     st.header("Sports Rehab Service")
                     df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Database')
-                    a = df.groupby(['IC NUMBER','NAME'])['NAME'].count()
+                    type = df['IC NUMBER'].astype(str)
+                    a = type.groupby(['IC NUMBER','NAME'])['NAME'].count()
                     st.write(a)
                   
                    
