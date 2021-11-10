49a5d3d1d4e89d524c7308a145a132879555e4fb
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-10 09:59:55 +0800

    Update App.py

--- App.py
+++ App.py
@@ -114,7 +114,7 @@ def main():
                    st.subheader('Cases by Sports' + '(' +month +'/'+year+')') 
                    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))    
                    ax.pie(y,labels=y)
-                   ax.legend(y, loc='best', bbox_to_anchor=(1.05, 1.0))
+                   ax.legend(mylabels, loc='best', bbox_to_anchor=(1.05, 1.0))
                    fig.tight_layout()
                    st.pyplot(fig)
                 
