a546977fb9383ad19b98ac873f81b016647fcdd2
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-16 21:27:31 +0800

    Update App.py

--- App.py
+++ App.py
@@ -151,10 +151,10 @@ def main():
                     sheet_id = st.secrets[username]
                     st.header("Supplement")
                     df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Sheet1')
-                    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True, utc=None)
+                    DATE = pd.to_datetime(df['Date'], dayfirst=True, utc=None)
                     startdate = st.text_input("Chose the start date")
                     enddate = st.text_input("Chose the end date")
-                    mask = (df['Date'] > startdate) & (df['Date'] <= enddate)
+                    mask = (DATE['Date'] > startdate) & (DATE['Date'] <= enddate)
                     wholedata = df.loc[mask]
                     st.write(wholedata)
                  
