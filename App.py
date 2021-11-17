4080772e2fdb976a3bdfdf2410e406ac1bfa54b1
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-17 14:39:57 +0800

    Update App.py

--- App.py
+++ App.py
@@ -181,7 +181,7 @@ def main():
                     sportchosen = st.selectbox('Chose the sport', wholedata['Sports'].drop_duplicates())
                     selectedsport = wholedata.loc[df['Sports']==sportchosen]
                     filtersport = selectedsport.groupby(['Supplement','Name'])['Quantity','Total Price (RM)'].sum()
-                    st.table(filtersport)
+                    st.write(filtersport)
                     
                  
 
