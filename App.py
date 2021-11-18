55691e0ec58d6a39a9ca11df864d896050d4a441
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-18 21:19:05 +0800

    Update App.py

--- App.py
+++ App.py
@@ -153,7 +153,7 @@ def main():
                if password == st.secrets["password"]:
                     sheet_id = st.secrets[username]
                     st.header("Attendance")
-                    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Sheet1')
+                    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Attendance')
                     st.write(df)
     
     
