ece136d6cf9f18ba9ff06ffcce8ffa42c1d83bd9
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-27 18:01:02 +0800

    Update App.py

--- App.py
+++ App.py
@@ -163,6 +163,7 @@ def main():
                    cs = chosen_sport.groupby(['Month','Name'],sort=False, as_index=False)['Case'].count()
                    cs2 = chosen_sport2.groupby(['Month'],sort=False)['Case'].count()
                    st.line_chart(cs2)
+                   st.table(cs2)
                    st.table(cs)
         
                   
