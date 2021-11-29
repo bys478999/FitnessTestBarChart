7742df6f3ed8240488e68cf1903527ec44cb46a1
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-11-29 12:09:25 +0800

    Update App.py

--- App.py
+++ App.py
@@ -340,7 +340,7 @@ def main():
           if st.sidebar.checkbox("Login"): 
                if password == st.secrets["password"]:
                     sheet_id = st.secrets[username]
-                    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Sheet 2')
+                    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx", sheet_name='Database')
                     st.write(df)
                  
 
