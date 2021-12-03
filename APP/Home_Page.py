74667f1399792b3b49ba89eda3474d95f7e2030e
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-03 22:26:10 +0800

    Update Home_Page.py

--- APP/Home_Page.py
+++ APP/Home_Page.py
@@ -23,7 +23,7 @@ def app():
             elif y == 'BMI':
                 sorted_y = df.sort_values(by=['BMI'], inplace=True)
                 x = df['NAME']
-                y = round(df['BMI'].astype(float), 2)
+                y = round(sorted_y['BMI'].astype(float), 2)
                 cc = ['colors'] * len(y)
                 a_BMI = 18.5
                 b_BMI = 18.5
