fb18bbc3c997f4e01ce607078cd770510306b75e
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-04 21:48:14 +0800

    Update Home_Page.py

--- APP/Home_Page.py
+++ APP/Home_Page.py
@@ -25,6 +25,7 @@ def app():
                 st.dataframe(sorted_y)
                 x = sorted_y['NAME']
                 y = round(sorted_y['BMI'].astype(float), 2)
+                st.dataframe(y)
                 cc = ['colors'] * len(y)
                 a_BMI = 18.5
                 b_BMI = 18.5
