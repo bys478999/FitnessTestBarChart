7bc3442c6117302139310bfca162353430d11886
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-04 10:07:38 +0800

    Update Home_Page.py

--- APP/Home_Page.py
+++ APP/Home_Page.py
@@ -12,9 +12,7 @@ def app():
     file = st.file_uploader("Chose a file")
     if file is not None:
         df = pd.read_excel(file)
-        sortedy = df.sort_values(by=['BMI'], inplace=True)
         st.dataframe(round(df, 2))
-        st.dataframe(round(sortedy, 2))
         st.sidebar.header('Fitness Test')
         y_value = st.sidebar.selectbox('Test', df.columns)
 
