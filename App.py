09933524dfa4f7c3776baa7f6ec855a3b43a0231
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-08 21:38:39 +0800

    Update App.py

--- App.py
+++ App.py
@@ -348,10 +348,10 @@ def main():
                     startdate = pd.to_datetime(startdate)
                     enddate = col2.date_input("Chose the end date(year/month/day):")
                     enddate = pd.to_datetime(enddate)
-                    #period = df[(df['DATE'] >= startdate) & (df['DATE'] <= enddate)]   
-                    #selected_period = df.loc(period)
+                    period = df[(df['DATE'] >= startdate) & (df['DATE'] <= enddate)]   
+                    selected_period = df.loc(period)
                     st.subheader("Assessment and Treatment")
-                    st.write(type(startdate))
+                    st.write(type(selected_period))
                     st.subheader("Rehabilitation")
                     st.write(type(enddate))
                     st.write(df['DATE'])
