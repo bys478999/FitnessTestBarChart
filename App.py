961851290b8924f7eaa01e4d00b0fd0185e12522
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-11 16:26:47 +0800

    Update App.py

--- App.py
+++ App.py
@@ -174,8 +174,10 @@ def main():
                    st.pyplot(fig)
                    st.bar_chart(cs3)
                    paid = cs4 * 100
+                   total_cost = paid.sum()
                    st.subheader("Total Cost (RM)")
                    st.table(paid)
+                   st.table(total_cost)
 
    
                   
