3beabb96603cb42ec041ac29c1a80f08c8d1d824
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-24 12:42:24 +0800

    Update App.py

--- App.py
+++ App.py
@@ -180,7 +180,7 @@ def main():
                    st.subheader('Common Injury Part' + '(' +month +'/'+year+')') 
                    y5 = selected_month.groupby(['Injury Part'])['Month'].count()
                    p6 = selected_month.groupby('Injury Part').groups
-                   fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 6))   
+                   fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))   
                    ax.pie(y5, colors = color, labels=y5, autopct='%1.1f%%', pctdistance=1.1, labeldistance= 0.7, textprops={'fontsize': 8})
                    ax.legend(p6, loc='best', bbox_to_anchor=(1.05, 1.0), fontsize='xx-small')
                    fig.tight_layout()
