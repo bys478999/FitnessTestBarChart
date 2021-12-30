d56c24729bea8814443c5950a53b732d4923d207
bys478999 <90168126+bys478999@users.noreply.github.com>
2021-12-30 11:04:16 +0800

    Update Third_Page.py

--- APP/Third_Page.py
+++ APP/Third_Page.py
@@ -12,7 +12,7 @@ def app():
     if file_2 is not None:
         df = pd.read_excel(file_1)
         df1 = pd.read_excel(file_2)
-        a = (df.merge(df1, left_on='NAME', right_on='NAME',suffixes=('_1', '_2'), how='outer'))
+        a = df.merge(df1, left_on='NAME', right_on='NAME',suffixes=('_1', '_2'), how='outer')
         st.write(a)
         st.download_button(label='Download the dataframe',data=a.to_csv(),mime='text/csv', file_name='Pre & Post Test Comparison.csv')
         
