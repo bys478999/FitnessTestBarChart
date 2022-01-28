df84fa4d5fa9f7e1527de3cd3e9dc5d9a30a5a0a
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-01-28 09:45:17 +0800

    Update Sixth_Page.py

--- APP/Sixth_Page.py
+++ APP/Sixth_Page.py
@@ -9,9 +9,6 @@ import openpyxl
 
 def app():
     st.title('Fitness Test Result')
-    st.text_input("Pasword")
-    psw = st.text_input("Password", type='password')
-    if psw == st.secrets["password_1"]:
-        sheet_id = st.secret[fitness_test]
-        df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx")
-        st.write(df)
+    sheet_id = st.secret[fitness_test]
+    df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx")
+    st.write(df)
