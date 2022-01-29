2d47a837a323e529971da82ceb749a5615ed69de
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-01-29 12:10:02 +0800

    Update Sixth_Page.py

--- APP/Sixth_Page.py
+++ APP/Sixth_Page.py
@@ -14,24 +14,53 @@ def app():
         sheet_id = st.secrets["fitness_test"]
         df = pd.read_excel(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx")
         st.write(df)
-        bodyfat = df[['YEAR','SPORT','NAME','GENDER','AGE','BODY FAT']]
-        bf = bodyfat.dropna()
+        bf = df[['YEAR','SPORT','NAME','GENDER','AGE','BODY FAT']].dropna()
         st.subheader('Body Fat (%)')
         st.write(bf)
         
-        maximumpushup = df[['YEAR','SPORT','NAME','GENDER','AGE','MAXIMUM PUSH UP']]
-        mpu = maximumpushup.dropna()
+        mpu = df[['YEAR','SPORT','NAME','GENDER','AGE','MAXIMUM PUSH UP']].dropna()
         st.subheader('Maximum Push Up (Repetition)')
         st.write(mpu)
         
-        situp = df[['YEAR','SPORT','NAME','GENDER','AGE','1 MIN SIT UP']]
-        su = situp.dropna()
+        su = df[['YEAR','SPORT','NAME','GENDER','AGE','1 MIN SIT UP']].dropna()
         st.subheader('1 Minute Sit Up (Repetition)')
         st.write(su)
         
         sbj = df[['YEAR','SPORT','NAME','GENDER','AGE','SBJ']].dropna()
         st.subheader('Standing Broad Jump (cm)')
         st.write(sbj)
+        
+        cmj = df[['YEAR','SPORT','NAME','GENDER','AGE','CMJ']].dropna()
+        st.subheader('Counter Movement Jump (cm)')
+        st.write(cmj)
+        
+        agility = df[['YEAR','SPORT','NAME','GENDER','AGE','ILLINOIS']].dropna()
+        st.subheader('Illinois Test (s)')
+        st.write(agility)
+        
+        sr = df[['YEAR','SPORT','NAME','GENDER','AGE','SIT & REACH']].dropna()
+        st.subheader('Sit & Reach (cm)')
+        st.write(sr)
+        
+        10 = df[['YEAR','SPORT','NAME','GENDER','AGE','10 M SPRINT']].dropna()
+        st.subheader('10 Meter Sprint (s)')
+        st.write(10)
+        
+        20 = df[['YEAR','SPORT','NAME','GENDER','AGE','20 M SPRINT']].dropna()
+        st.subheader('20 Meter Sprint (s)')
+        st.write(20)
+        
+        40 = df[['YEAR','SPORT','NAME','GENDER','AGE','40 M SPRINT']].dropna()
+        st.subheader('40 Meter Sprint (s)')
+        st.write(40)
+        
+        total = df[['YEAR','SPORT','NAME','GENDER','AGE','TOTAL']].dropna()
+        st.subheader('Total Handgrip Strength (kg)')
+        st.write(total)
+        
+        yy = df[['YEAR','SPORT','NAME','GENDER','AGE','YOYO']].dropna()
+        st.subheader('Beep Test')
+        st.write(yy)
     
     else:
         st.warning("Incorrect Password")
