74df8912ca1cd4f0181cf5e87add189cabc9cb7c
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-05-25 15:27:06 +0800

    Update App.py

--- App.py
+++ App.py
@@ -546,11 +546,10 @@ def main():
                     bar_2 = the_sport.groupby(['NAME'])['SPORT'].count()
                     st.subheader("Service Given (Athlete) for "+chosen_sport)
                     st.bar_chart(bar_2) 
-                    
-                    test = selected_period.sort_values(by=[''], inplace=False, ignore_index=True) 
+
                     Test = ["IMAGERY", "MENTAL PREPARATION", "SELF-CONFIDENCE", "ANXIETY CONTROL", "CONCENTRATION", "RELAXATION ABILITY", "MOTIVATION"]
                     choice = st.selectbox("Chose The Test",Test)
-                    sorted_y = the_sport.sort_values(by=choice, inplace=False, ignore_index=True) 
+                    sorted_y = the_sport.sort_values(by=[choice], inplace=False, ignore_index=True) 
                     width = st.sidebar.slider("plot width", 1., 15., 10.)
                     height = st.sidebar.slider("plot height", 1., 10., 5.)
                     xx = st.sidebar.slider("bottom Y-axis", 0., 40., 0.)
