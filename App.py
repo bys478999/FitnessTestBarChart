30915d4a78feca1389740ab7e81a513c0a822408
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-06-03 16:15:52 +0800

    Update App.py

--- App.py
+++ App.py
@@ -572,25 +572,8 @@ def main():
                     df1 = pd.DataFrame(data=the_sport)
                     df2 = df1.T
                     st.write(df2)
-                    the_sport_2 = the_sport.T.copy
-                    st.write(the_sport_2)
-                    sorted_y = the_sport.sort_values(by=[choice], inplace=False, ignore_index=True) 
-                    width = st.sidebar.slider("plot width", 1., 20., 15.)
-                    height = st.sidebar.slider("plot height", 1., 10., 5.)
-                    xx = st.sidebar.slider("bottom Y-axis", 0., 40., 0.)
-                    yy = st.sidebar.slider("upper Y-axis", 0., 120., 100.)
-                    x = sorted_y['NAME'] 
-                    y = round(sorted_y[choice].astype(float), 2)
-                    cc =  ['#3354FF', '#50FF33','#FFFE33','#33FFB7','#f00505','#8733FF','#C533FF','#FF9333','#B6FF33','#33FF7F','#721601','#988943','#858984','#3F832E','#2D776A','#015089','#7567A9','#4B4A4E','#34600E','#E3A951']
-                    fig, ax = plt.subplots(figsize=(width, height))
-                    ax = plt.bar(x, y, data=y, color=cc, width=0.5) 
-                    for i in range(len(x)):
-                        plt.text(i, y[i], y[i], ha="center", va="bottom", fontsize="medium")
-                    plt.xticks(rotation='vertical', fontsize="medium", ha="right", va="center", wrap=True)
-                    plt.title(choice)
-                    plt.ylabel(choice+" SCORE")
-                    plt.ylim(xx, yy)
-                    st.pyplot(fig)
+
+
                   
                      
 if __name__ == '__main__':
