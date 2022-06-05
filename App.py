957a1e9d4ac53d7abdfde0370389b26076668d50
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-06-05 12:47:44 +0800

    Update App.py

--- App.py
+++ App.py
@@ -574,7 +574,7 @@ def main():
                     st.write(the_athlete)
                     df2 = pd.DataFrame(data=the_athlete)
                     df1 = str(df2.transpose())
-                    st.dataframe(df1)
+                    st.write(df1)
 
                   
                      
