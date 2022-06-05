fa0ac9077775f25f64773e8ffeb0d8934428fe4c
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-06-05 12:17:28 +0800

    Update App.py

--- App.py
+++ App.py
@@ -569,7 +569,7 @@ def main():
                            
                     newdata = the_sport.drop('DATE', axis=1, inplace=True)
                     choice = st.selectbox("Chose The Athlete",the_sport['NAME'].drop_duplicates())
-                    the_athlete = the_sport.loc[df['NAME']==choice]
+                    the_athlete = the_sport.loc[df['NAME']==choice].set_index('NAME').head()
                     st.write(the_sport)
                     st.write(the_athlete)
                     df1 = str(the_athlete.T)
