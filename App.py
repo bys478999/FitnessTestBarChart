8a607e14828fcc2f79e06a5f24d43a7419bdf5c0
bys478999 <90168126+bys478999@users.noreply.github.com>
2022-06-15 09:51:42 +0800

    Update App.py

--- App.py
+++ App.py
@@ -580,7 +580,7 @@ def main():
                     height_1 = st.slider("height", 1., 10., 5.)
                     xx1 = st.slider("bottom axis", 0., 40., 0.)
                     yy1 = st.slider("upper axis", 0., 120., 120.)
-                    x = sorted_y['LEE CHIA YEE'] 
+                    x = sorted_y[choice] 
                     y = round(sorted_y['NAME'].astype(float), 2)
                     cc =  ['#3354FF', '#50FF33','#FFFE33','#33FFB7','#f00505','#8733FF','#C533FF','#FF9333','#B6FF33','#33FF7F','#721601','#988943','#858984','#3F832E','#2D776A','#015089','#7567A9','#4B4A4E','#34600E','#E3A951']
                     fig, ax = plt.subplots(figsize=(width_1, height_1))
